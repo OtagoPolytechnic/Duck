8c174800d5f35621faf15b2414137510f0ab1c95
rohan <rohan.anakin@gmail.com>
2023-12-05 03:06:35 +1300

    feat: add onpickup method, heal and damage item

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -10,4 +10,31 @@ public abstract class Item
     {
 
     }
+    public virtual void OnPickup(int level) //requires player connection to be able to be called and used i.e (Player player)
+    {
+
+    }
+}
+
+public class Healonhit : Item
+{
+    public override string Name()
+    {
+        return "Heal on hit";
+    }
+    public override void Update(int level) //also needs the above player connection
+    {
+        //player.health += 5 * level;
+    }
+}
+public class DamageIncrease : Item
+{
+    public override string Name()
+    {
+        return "Damage Increase";
+    }
+    public override void OnPickup(int level) //also needs the above player connection
+    {
+        //player.damage += player.damage * level;
+    }
 }
