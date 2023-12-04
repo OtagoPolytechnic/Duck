5bfdff307086e1e3732e935056516caa98aee92c
rohan <rohan.anakin@gmail.com>
2023-12-05 11:47:44 +1300

    feat: add rarity, speed item

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public abstract class Item
 {
     public abstract string Name();
+    public abstract string Rarity();
     public virtual void Update(int level) //requires player connection to be able to be called and used i.e (Player player)
     {
 
@@ -22,6 +23,10 @@ public class Healonhit : Item
     {
         return "Heal on hit";
     }
+    public override string Rarity()
+    {
+        return "Common";
+    }
     public override void Update(int level) //also needs the above player connection
     {
         //player.health += 5 * level;
@@ -33,8 +38,27 @@ public class DamageIncrease : Item
     {
         return "Damage Increase";
     }
+    public override string Rarity()
+    {
+        return "Common";
+    }
     public override void OnPickup(int level) //also needs the above player connection
     {
         //player.damage += player.damage * level;
+    }
+}
+public class SpeedIncrease : Item
+{
+    public override string Name()
+    {
+        return "Speed Increase";
+    }
+    public override string Rarity()
+    {
+        return "Common";
+    }
+    public override void OnPickup(int level) //also needs the above player connection
+    {
+        //player.speed += player.speed * level;
     }
 }
