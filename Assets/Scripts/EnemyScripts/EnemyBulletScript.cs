2a8afff41b1ee5902843a72a0790b60cca02f3f8
hartlr3 <lrhart42@gmail.com>
2024-02-14 12:53:43 +1300

    feat: add logic to destroy bullet after certain time

--- Assets/Scripts/EnemyScripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyBulletScript.cs
@@ -7,7 +7,7 @@ public class EnemyBulletScript : MonoBehaviour
     public GameObject player;
     private Rigidbody2D rb;
     public float fireRate;
-
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,12 @@ public class EnemyBulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //destroys bullet after time elapsed
+        timer += Time.deltaTime;
+
+        if (timer >10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
