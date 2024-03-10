755f831ca96a7e1d8bb012b57ec514a545441564
hartlr3 <lrhart42@gmail.com>
2024-03-10 21:00:14 +1300

    chore: add Game Manager script and object for game over methods

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+   bool playerDead = false;
+   public void GameOver()
+   {
+        if (!playerDead)
+        {
+            playerDead = true;
+            gameOverUI.SetActive(true);
+            Debug.Log("Game Over");
+        }
+        
+   }
+}
