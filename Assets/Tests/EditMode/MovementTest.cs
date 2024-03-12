b0cc3b2caad1b29e5c3ad0e0fda81e6d0443e718
unknown <xushen006@gmail.com>
2024-03-12 21:53:14 +1300

    add a test code in

--- Assets/Tests/EditMode/MovementTest.cs
+++ Assets/Tests/EditMode/MovementTest.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -11,10 +10,25 @@ public class MovementTest
     public IEnumerator MovementTestWithEnumeratorPasses()
     {
 
-        GameObject gameobject = new GameObject();
-        
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        // Setup
+        GameObject gameObject = new GameObject();
+        Rigidbody2D rb2d = gameObject.AddComponent<Rigidbody2D>();
+        TopDownMovement topDownMovement = gameObject.AddComponent<TopDownMovement>();
+        topDownMovement.moveSpeed = 5f; // Assuming a speed
+
+        // Simulate input
+        float simulatedVerticalInput = 1.0f; // Simulate moving up
+        topDownMovement.TestInput(new Vector2(0, simulatedVerticalInput)); // You need to create this method in TopDownMovement
+
+        // Wait a frame for movement to apply
         yield return null;
+
+        // Assert
+        Assert.Greater(rb2d.velocity.y, 0);
+
+        // Cleanup
+        Object.Destroy(gameObject);
+        
+        
     }
 }
