d8b5c3f18b8b56884634ddaf8b7bfde3e8e9b292
YJunX <xushen006@gmail.com>
2024-03-18 14:34:21 +1300

    fixed some code

--- Assets/Scripts/WaveManagement/Timer.cs
+++ Assets/Scripts/WaveManagement/Timer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI waveNumberText;
--- Assets/Tests/playerMovementTest.cs
+++ Assets/Tests/playerMovementTest.cs
@@ -3,30 +3,27 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class MovementTest
+public class playerMovementTest
 {
     
     [UnityTest]
     public IEnumerator MovementTestWithEnumeratorPasses()
     {
 
-        // Setup
+       
         GameObject gameObject = new GameObject();
         Rigidbody2D rb2d = gameObject.AddComponent<Rigidbody2D>();
         TopDownMovement topDownMovement = gameObject.AddComponent<TopDownMovement>();
-        topDownMovement.moveSpeed = 5f; // Assuming a speed
+        topDownMovement.moveSpeed = 5f; 
 
-        // Simulate input
-        float simulatedVerticalInput = 1.0f; // Simulate moving up
-        topDownMovement.TestInput(new Vector2(0, simulatedVerticalInput)); // You need to create this method in TopDownMovement
-
-        // Wait a frame for movement to apply
+       
+        rb2d.velocity = new Vector2(5f, 0f);
+       
         yield return null;
 
-        // Assert
-        Assert.Greater(rb2d.velocity.y, 0);
+        Assert.AreEqual(new Vector2(5f, 0f), rb2d.velocity);
 
-        // Cleanup
+       
         Object.Destroy(gameObject);
         
         
