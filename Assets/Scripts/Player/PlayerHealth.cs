b78e7a7b95960bbcc7f6be14c6143d88d7882c79
AlexReidNZ <reidaj7@student.op.ac.nz>
2024-04-05 10:43:36 +1300

    feat: add ability for player to respawn at start position

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health;
+    public float maxHealth;
+
+    [HideInInspector]
+    public float currentHealth;
+
+    public int lives;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0)
+        if(currentHealth <= 0)
         {
-            FindObjectOfType<GameManager>().GameOver();
+            if (lives > 1)
+            {
+                Respawn();
+            }
+            else
+            {
+                FindObjectOfType<GameManager>().GameOver();
+            }
         }
+    }
+
+    void Respawn()
+    {
+        Debug.Log("respawned");
+        lives -= 1;
+        gameObject.transform.position = new Vector3(0,0,0); //Will use position of nest gameObject once it is added
+        currentHealth = maxHealth;
     }
 }
