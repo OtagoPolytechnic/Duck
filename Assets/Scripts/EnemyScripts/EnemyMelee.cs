190cab5230f85771497ddd646392a0b20c6597cf
AlexReidNZ <reidaj7@student.op.ac.nz>
2024-05-13 16:46:50 +1200

    chore: change method to coroutine

--- Assets/Scripts/EnemyScripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyScripts/EnemyMelee.cs
@@ -9,6 +9,7 @@ public class EnemyMelee : MonoBehaviour
     private float distance;
     private float attackRange;
     private MapManager mapManager;
+    private bool attacking = false;
 
     private void Awake()
     {
@@ -28,17 +29,26 @@ public class EnemyMelee : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, (speed * tileSpeedModifier) * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        if (!attacking) //This enemy type stops moving to attack
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, (speed * tileSpeedModifier) * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
         
         if (distance <= attackRange)
         {
-            Attack();
+            StartCoroutine(Attack());
         }
     }
 
-    void Attack()
+    IEnumerator Attack()
     {
-
+        bool attackFinished = false;
+        //do the attack
+        if (attackFinished)
+        {
+            StopCoroutine(Attack());
+        }
+        yield return null;
     }
 }
