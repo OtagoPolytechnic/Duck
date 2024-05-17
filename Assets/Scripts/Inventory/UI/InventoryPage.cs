672e57edc9f5200f4499fc70ef9b01b1df3c1416
Rohan Laptop <rohan.anakin@gmail.com>
2024-05-17 16:11:48 +1200

    feat: add item weighting, currently dupelicates

--- Assets/Scripts/Inventory/UI/InventoryPage.cs
+++ Assets/Scripts/Inventory/UI/InventoryPage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 [System.Serializable]
 public class Item
@@ -26,6 +27,7 @@ public class InventoryPage : MonoBehaviour
     private string randomItemName;
     private string randomItemDesc;
     private rarity randomItemRarity;
+    public rarity roll;    
     //if you change something in this list you need to change it in ItemController.cs's method ItemPicked()
     public static List<Item> itemList = new List<Item>{ //char limit of 99 in description 
         new() { name = "Damage Increase", desc = "Increases damage you deal", rarity = rarity.Common },
@@ -44,25 +46,73 @@ public class InventoryPage : MonoBehaviour
 
     List<InventoryItem> preGenItems = new List<InventoryItem>();
 
+    public rarity GetWeightedRarity() {
+        // Define some thresholds for different item rarities. (between 0 and 1)
+        float commonRoll = 0.4f;
+        float uncommonRoll = 0.65f;
+        float rareRoll = 0.8f;
+        float epicRoll = 0.95f;    
+        // generate a random value (0->1)
+        float genRarity = UnityEngine.Random.value;
+        // Figure out which threshold this falls under.
+        if (genRarity < commonRoll)
+        {
+            roll = rarity.Common;
+        }
+        else if (genRarity < uncommonRoll)
+        {
+            roll = rarity.Uncommon;
+        }
+        else if (genRarity < rareRoll)
+        {
+            roll = rarity.Rare;
+        }
+        else if (genRarity < epicRoll)
+        {
+            roll = rarity.Epic;
+        }
+        // Return the rarity associated with that threshold.
+        return roll;
+    }
+
     public void InitializeInventoryUI(int inventorySize) //this is called every time the item ui pops up
     { 
         List<Item> tempItems = new List<Item>(itemList);
+        List<Item> generatedRarityList = new List<Item>();
+        
         for (int i = preGenItems.Count - 1; i >= 0; i--) //makes sure the items previously generated are cleared to not bunch up on the inventory window
         {
             Destroy(preGenItems[i].gameObject); //removes item
             preGenItems.RemoveAt(i); //removes floating null pointer
         } 
 
-        for (int i = 0; i < inventorySize; i++) //generates an item without duplication an assigns it to the prefab.
+        for (int i = 0; i < inventorySize; i++) //because of the new weight code, this now has dupelicates back
         {
-            index = UnityEngine.Random.Range(0, tempItems.Count); 
-            randomItemName = tempItems[index].name;
-            randomItemDesc = tempItems[index].desc;
-            randomItemRarity = tempItems[index].rarity;
+            // Get a random rarity.
+            randomItemRarity = GetWeightedRarity();
+            // Create a list of all the available items of that rarity.
+            foreach (Item rarity in tempItems)
+            {
+                Debug.Log(rarity.name);
+                if (rarity.rarity == randomItemRarity)
+                {
+                    generatedRarityList.Add(rarity);
+                }
+            }
+
+            // then pick a random index from that subset and use that as the item.
+            index = UnityEngine.Random.Range(0, generatedRarityList.Count); 
+
+            randomItemName = generatedRarityList[index].name;
+            randomItemDesc = generatedRarityList[index].desc;
+            randomItemRarity = generatedRarityList[index].rarity;
+
             Debug.Log($"In InventoryPage.cs: index chosen is {index} and item is {randomItemName}");
+            generatedRarityList.RemoveAt(index);
             tempItems.RemoveAt(index);
             InventoryItem item = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);            
             preGenItems.Add(item);
+
             item.GetComponent<InventoryItem>().itemName = randomItemName;
             item.GetComponent<InventoryItem>().itemDesc = randomItemDesc;
             item.GetComponent<InventoryItem>().itemRarity = randomItemRarity;
