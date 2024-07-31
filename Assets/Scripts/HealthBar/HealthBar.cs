65f2f4a68caece91c1c99e2394b58ebc845daeca
rohan <rohan.anakin@gmail.com>
2024-08-01 01:46:15 +1200

    refactor: rework health text code to new UI system

--- Assets/Scripts/HealthBar/HealthBar.cs
+++ Assets/Scripts/HealthBar/HealthBar.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class HealthBar : MonoBehaviour
 {
-    public Text healthText;
-    private Image healthBar;
-
+    public Label healthText;
+    //private UnityEngine.UI.Image healthBar; //temp
+    void Awake()
+    {
+        VisualElement document = GetComponent<UIDocument>().rootVisualElement;
+        healthText = document.Q("HealthNumber") as Label;
+    }
     void Start()
     {
-        healthBar = GetComponent<Image>();
+        //healthBar = GetComponent<UnityEngine.UI.Image>();
     }
 
     void Update()
     {
         float healthFraction = PlayerHealth.currentHealth / PlayerHealth.maxHealth;
-        healthBar.fillAmount = healthFraction;
+        //healthBar.fillAmount = healthFraction;
         healthText.text = PlayerHealth.currentHealth.ToString("F0") + "/" + PlayerHealth.maxHealth.ToString("F0");
     }
 }
