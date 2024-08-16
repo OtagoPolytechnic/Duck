439bc3ba1dc525874f321353668f6d29fa0435f6
PBWiseman <wisepb1@student.op.ac.nz>
2024-08-16 13:32:44 +1200

    FIX: Tutorial grammar errors

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -12,7 +12,7 @@ public class Tutorial : MonoBehaviour
         goBack.RegisterCallback<ClickEvent>(ReturnToMainMenu);
 
         Label paragraph = document.Q<Label>("Paragraph");
-        paragraph.text = "WASD to move. Hold left click to shoot. after each wave you get a choice of item. see how long you can survive";
+        paragraph.text = "WASD to move. Hold left click to shoot.\nAfter each wave you get a choice of item.\nSee how long you can survive!";
     }
     private void ReturnToMainMenu(ClickEvent click)
     {
