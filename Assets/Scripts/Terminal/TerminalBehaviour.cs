ab6ea6561678830882f6d8598b3543bcb49aafa4
rohan <rohan.anakin@gmail.com>
2024-08-30 01:19:43 +1200

    feat: add giveitem and setwave commands

--- Assets/Scripts/Terminal/TerminalBehaviour.cs
+++ Assets/Scripts/Terminal/TerminalBehaviour.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
-using UnityEditor;
+using System;
 
 public class TerminalBehaviour : MonoBehaviour
 {
-
     private Label output;
     private TextField input;
     private VisualElement document;
     private VisualElement terminalWindow;
+    [SerializeField]
+    private ItemEffectTable effectTable;
 
     
     void Awake()
@@ -54,8 +54,14 @@ public class TerminalBehaviour : MonoBehaviour
                 case "setweapon":
                     SetWeapon(commands[1]);
                 break;
+                case "giveitem":
+                    GiveItem(commands[1], commands[2]);
+                break;
+                case "setwave":
+                    SetWave(commands[1]);
+                break;
                 default:
-                    output.text += "ERROR: Not a command\n";
+                    output.text += "Not a valid command\n";
                 break;
             }   
             input.value = "";
@@ -66,8 +72,8 @@ public class TerminalBehaviour : MonoBehaviour
     public void Help()
     {
         output.text += "\nAvailable Commands: \n\nsetweapon {weaponName} | will set player weapon to weapon given. Accepted values: pistol, dualpistol, shotgun, sniper, machinegun, rocket, sword\n\n" +
-        "giveitem {itemID} | will give an item from the item list to the player (will not add to list of items the player has)\n\n" +
-        "setwave {waveNo.} | sets the wave to specified number\n\n" +
+        "giveitem {itemID} {amount} | will give an item or multiple from the item list to the player (will not add to list of items the player has)\n\n" +
+        "setwave {waveNo.} | sets the next wave to the specified number\n\n" +
         "setstat {stat} {value} | sets the stat specified to the value given. Accepted stats: damage, crit, maxhealth, firedelay, movespeed, critdamage\n\n" +
         "spawn {enemyId} {count} | spawns an enemy with the id given and the amount given\n\n" +
         "god | sets and unsets the player from god mode\n\n";
@@ -97,7 +103,39 @@ public class TerminalBehaviour : MonoBehaviour
             case "sword":
                 WeaponStats.Instance.CurrentWeapon = WeaponType.Pistol;
             break;
+            default:
+                output.text += "Invalid weapon name";
+            break;
         }
-        output.text += $"Weapon set to {WeaponStats.Instance.CurrentWeapon}\n";
+        output.text += $"Weapon set to {WeaponStats.Instance.CurrentWeapon}\n\n";
+    }
+    private void GiveItem(string itemId, string itemAmount)
+    {
+        if (!int.TryParse(itemId, out int id))
+        {
+            output.text += "ItemId given is incorrect";
+            return;
+        }
+        if (!int.TryParse(itemAmount, out int amount))
+        {
+            output.text += "Amount given is no a number";
+            return;
+        }
+        for (int i = 0; i < amount; i++)
+        {
+            effectTable.ItemPicked(id); //activate the item selected's code
+        }
+        output.text += $"Added {amount} {ItemPanel.itemList[id].name} to player\n\n";
+    }
+    private void SetWave(string wave)
+    {
+        if (!int.TryParse(wave, out int waveNumber))
+        {
+            output.text = "Wave number given is not a number";
+            return;
+        }
+        GameSettings.waveNumber = waveNumber - 1;
+        Timer.Instance.waveNumber = waveNumber - 1;
+        output.text += $"Next wave set to {GameSettings.waveNumber + 1}";
     }
 }
