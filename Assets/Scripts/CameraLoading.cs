4092c39484fd8f506f99858c68b86f188b15da44
PBWiseman <wisepb1@student.op.ac.nz>
2024-09-17 16:42:17 +1200

    FIX: Camera aiming issues

--- Assets/Scripts/CameraLoading.cs
+++ Assets/Scripts/CameraLoading.cs
@@ -7,9 +7,16 @@ public class CameraLoading : MonoBehaviour
     //This code just makes sure there there is only ever 1 event system no matter where you load the game from
     [HideInInspector]
     public static CameraLoading instance;
+    public bool isMainCamera = false; //This is to make sure that the main camera is kept instead of a menu one
     void Awake()
     {
-        if (instance == null)
+        if (isMainCamera && instance != null) //If this is the main camera and there is already another camera, destroy it
+        {
+            Destroy(instance.gameObject);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
