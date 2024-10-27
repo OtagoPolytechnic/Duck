ba8061d9389934b731b0485cbf424382c173accc
PBWiseman <wisepb1@student.op.ac.nz>
2024-10-28 12:57:19 +1300

    FIX: Line breaks in credits

--- Assets/Scripts/Menus/Credits.cs
+++ Assets/Scripts/Menus/Credits.cs
@@ -17,7 +17,7 @@ public class Credits : MonoBehaviour
         Label paragraph = document.Q<Label>("Paragraph");
         paragraph.text = "Developers:\n\nAlex Reid\nKyle Black\nRohan Anakin\nPalin Wiseman\n\n" +
         "Former developers:\n\nLorna Hart\nJun Xu\n\n" +
-        "Music:\n\n \"8Bit Music - 062022\"  -  GWriterStudio" +
+        "Music: \"8Bit Music - 062022\"  -  GWriterStudio\n\n" +
         "Reroll icon by Stephen Kerr from Noun Project (CC BY 3.0) https://thenounproject.com/browse/icons/term/reroll/ ";
     }
 
