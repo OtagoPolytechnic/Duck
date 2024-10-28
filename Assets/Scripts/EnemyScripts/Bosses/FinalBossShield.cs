a0f226f69b4fe2232e02f564d2e68d3bf168cff3
Kyle Black (1000026735) <BLACK11@student.op.ac.nz>
2024-10-28 17:41:12 +1300

    Changing boss shield visuals and shield behaviour

--- Assets/Scripts/EnemyScripts/Bosses/FinalBossShield.cs
+++ Assets/Scripts/EnemyScripts/Bosses/FinalBossShield.cs
@@ -5,22 +5,14 @@ using UnityEngine;
 public class FinalBossShield : MonoBehaviour
 {
   
-    public float pushDistance = 2f;
+
 
     void Start()
     {
       
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            Vector2 direction = (other.transform.position - transform.position).normalized;
-
-            other.transform.position += (Vector3)direction * pushDistance;
-        }
-    }
+ 
 
     public void TakeDamage()
     {
